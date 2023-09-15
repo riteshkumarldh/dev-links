ccb6ac7ea236858036857915876b31f0201ffb70
Ritesh <ritesh.comforttechno@gmail.com>
2023-09-15 22:24:08 +0530

    refactor code

--- src/components/ProfileLinksPreview.jsx
+++ src/components/ProfileLinksPreview.jsx
@@ -1,3 +1,11 @@
+import ProfileCard from "./ProfileCard";
+
 export default function ProfileLinksPreview() {
-  return <div>ProfileLinksPreview</div>;
+  return (
+    <div className="flex justify-center">
+      <div className="bg-white py-12 px-14 rounded-xl relative top-40 shadow-xl">
+        <ProfileCard />
+      </div>
+    </div>
+  );
 }
