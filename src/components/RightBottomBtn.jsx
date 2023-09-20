dc6fb41c304fe8a0646ec072362c26cab23865a5
Ritesh <ritesh.comforttechno@gmail.com>
2023-09-20 21:27:14 +0530

    btn disabled

--- src/components/RightBottomBtn.jsx
+++ src/components/RightBottomBtn.jsx
@@ -1,10 +1,13 @@
+import { useLink } from "../utils/uselink";
+
 import Button from "./Button";
 
-export default function RightBottomBtn({ onClick, disabled }) {
+export default function RightBottomBtn({ onClick }) {
+  const { links } = useLink();
   return (
     <div className="py-6 border-t border-t-gray-300 mt-auto w-full flex justify-end pr-10">
       <Button
-        disabled={disabled}
+        disabled={links.length < 1}
         onClick={onClick}
         type="submit"
         className="bg-bl-300 w-max px-6 text-white font-semibold hover:bg-bl-200 disabled:bg-bl-200 "
